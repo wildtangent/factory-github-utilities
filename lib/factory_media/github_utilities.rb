2080ec43c31ac42207e60119996d77d73beed241
Joe Connor <joe.connor@factorymedia.com>
2016-07-14 15:32:02 +0800

    move includes around

--- lib/factory_media/github_utilities.rb
+++ lib/factory_media/github_utilities.rb
@@ -1,8 +1,9 @@
-require "factory_media/github_utilities/version"
-require "factory_media/github_utilities/github_org_keys"
 
 module FactoryMedia
   module GithubUtilities
     # Your code goes here...
   end
 end
+
+require "factory_media/github_utilities/version"
+require "factory_media/github_utilities/github_org_keys"
